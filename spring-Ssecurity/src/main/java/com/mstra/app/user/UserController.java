964d5618c458c2e1a152cf3fe162989ef0ccec88
amosuzuagie <amosuzuagie@gmail.com>
2025-10-14 02:24:39 +0100

    feat(user): add UserController with endpoints for user management

---
+++ spring-Ssecurity/src/main/java/com/mstra/app/user/UserController.java
@@ -0,0 +1,57 @@
+package com.mstra.app.user;
+
+import com.mstra.app.user.request.ChangePasswordRequest;
+import com.mstra.app.user.request.ProfileUpdateRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/vi/users")
+public class UserController {
+    private final UserService userService;
+
+    @PatchMapping("/me")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void updateProfileInfo(
+            @RequestBody @Valid final ProfileUpdateRequest request,
+            final Authentication principal
+    ) {
+        this.userService.updateProfileInfo(request, getUserId(principal));
+    }
+
+    @PostMapping("/me/password")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void changePassword(
+            @RequestBody @Valid final ChangePasswordRequest request,
+            final Authentication principal
+    ) {
+        this.userService.changePassword(request, getUserId(principal));
+    }
+
+    @PatchMapping("/me/deactivate")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deactivateAccount(final Authentication principal) {
+        this.userService.deactivateAccount(getUserId(principal));
+    }
+
+    @PatchMapping("/me/reactivate")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void reactivateAccount(final Authentication principal) {
+        this.userService.reactivateAccount(getUserId(principal));
+    }
+
+    @DeleteMapping("/me")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deleteAccount(final Authentication principal) {
+        this.userService.deleteAccount(getUserId(principal));
+    }
+
+
+    private String getUserId(Authentication principal) {
+        return ((User) principal.getPrincipal()).getId();
+    }
+}
